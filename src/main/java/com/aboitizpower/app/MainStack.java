b07cb68f995bc78ffa530410697d9e3d99090aef
jay-ar-bautista-novare <jay-ar.bautista@novare.com.hk>
2022-09-13 23:13:21 +0800

    Create Server for Jenkins

--- src/main/java/com/aboitizpower/app/MainStack.java
+++ src/main/java/com/aboitizpower/app/MainStack.java
@@ -1,14 +1,15 @@
 package com.aboitizpower.app;
 
-import software.constructs.Construct;
-
-import com.hashicorp.cdktf.TerraformStack;
 import com.hashicorp.cdktf.TerraformOutput;
-
+import com.hashicorp.cdktf.TerraformStack;
+import com.hashicorp.cdktf.TerraformVariable;
 import com.hashicorp.cdktf.providers.aws.AwsProvider;
 import com.hashicorp.cdktf.providers.aws.ec2.Instance;
+import software.constructs.Construct;
 
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainStack extends TerraformStack
 {
@@ -19,10 +20,17 @@ public class MainStack extends TerraformStack
                 .region("ap-southeast-1")
                 .build();
 
+        TerraformVariable environment = TerraformVariable.Builder.create(this, "environment")
+                .type(String.class.getSimpleName().toLowerCase())
+                .build();
+
+        Map<String, String> tags = new HashMap<>();
+        tags.put("Name", environment.getStringValue() + "-JenkinsServer");
+
         Instance instance = Instance.Builder.create(this, "compute")
+                .tags(tags)
                 .ami("ami-0b89f7b3f054b957e")
                 .instanceType("t3.micro")
-                .keyName("jenkins_server")
                 .securityGroups(Collections.singletonList("sg-0541d523b2625fa85"))
                 .subnetId("subnet-07225cb9aa01bbb0f")
                 .build();
