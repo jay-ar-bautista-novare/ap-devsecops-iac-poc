9eeb1638d24ab2b3fbc518f8b18976a5b36ccbe4
jay-ar-bautista-novare <jay-ar.bautista@novare.com.hk>
2022-09-18 02:39:01 +0800

    Fix Jenkins is not installed

--- src/main/sh/install_docker.sh
+++ src/main/sh/install_docker.sh
@@ -2,9 +2,6 @@ sudo yum update -y
 sudo yum search docker
 sudo yum info docker
 sudo yum install docker -y
+sudo service docker start
 sudo usermod -a -G docker ec2-user
 id ec2-user
-newgrp docker << 'END'
-END
-sudo systemctl enable docker.service
-sudo systemctl start docker.service
